63ef3dd1cc9a84ce4788209fbe058b2908c56441
Emilano <azadvararvand@gmail.com>
2021-08-18 16:00:42 +0430

    Add files via upload

---
+++ lib/simple_cluster/application.ex
@@ -0,0 +1,35 @@
+defmodule SimpleCluster.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  @impl true
+  def start(_type, _args) do
+    children = [
+      {Cluster.Supervisor, [topologies(), [name: SimpleCluster.ClusterSupervisor]]},
+      SimpleCluster.Observer,
+      SimpleCluster.Ping
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: LibclusterCluster.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+
+  defp topologies do
+    [
+      example: [
+        startegy: Cluster.Startegy.Epmd,
+        config: [
+          hosts: [
+            :"n@127.0.0.1",
+            :"z@127.0.0.1",
+          ]
+        ]
+      ]
+    ]
+  end
+end
---
+++ mix.exs
@@ -0,0 +1,28 @@
+defmodule SimpleCluster.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :simple_cluster,
+      version: "0.1.0",
+      elixir: "~> 1.12",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger],
+      mod: {SimpleCluster.Application, []}
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:libcluster, "~> 3.3"}
+    ]
+  end
+end
